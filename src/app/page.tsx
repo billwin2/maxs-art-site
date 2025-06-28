429b7397ef0868b64dab3453e90d421a1adf5e28
billwin2 <bill.jessicawin@gmail.com>
2025-06-27 22:22:02 -0500

    Initial commit with basic setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Welcome to Max's Art Site!
+      </h1>
+    </main>
+  );
+}
