db41f02ef4cf1a753e8b70cc61ee962bc0a60f2a
billwin2 <bill.jessicawin@gmail.com>
2025-07-03 21:22:26 -0500

    Incorporated Gallery into home page and made some styling changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 'use client';
 
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Caveat } from "next/font/google";
@@ -24,24 +23,39 @@ export default function Home() {
         alt: "Painting of metallic cat",
       },
       {
+        src: "https://maxs-art-site.s3.us-east-2.amazonaws.com/flower_bou.JPG",
+        alt: "Beautiflul flowers on water"
+      },
+      {
         src: "https://maxs-art-site.s3.us-east-2.amazonaws.com/metal_flower.jpg",
         alt: "Painting of metallic flower"
       },
       {
         src: "https://maxs-art-site.s3.us-east-2.amazonaws.com/racoons.jpg",
         alt: "Painting of two racoons"
+      },
+      {
+        src: "https://maxs-art-site.s3.us-east-2.amazonaws.com/fish.JPG",
+        alt: "Underwater painting of school of fish"
+      },
+      {
+        src: "https://maxs-art-site.s3.us-east-2.amazonaws.com/frog_a.JPG",
+        alt: "frog from underside 1 of 2"
+      },
+      {
+        src: "https://maxs-art-site.s3.us-east-2.amazonaws.com/frog_b.JPG",
+        alt: "frog fron underside 2 of 2"
+      },
+      {
+        src: "https://maxs-art-site.s3.us-east-2.amazonaws.com/snail_toadstool.JPG",
+        alt: "Painting of snails on a toadstool"
+      },
+      {
+        src: "https://maxs-art-site.s3.us-east-2.amazonaws.com/moth.JPG",
+        alt: "Painting of a orange and white moth"
       }
+
     ];
-
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    // Switch images every 10 seconds
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 10000);
-      return () => clearInterval(interval);
-    }, [images.length]);
 
   return (
     <div className="flex flex-col h-screen">
@@ -64,36 +78,40 @@ export default function Home() {
           {/* Navigation Buttons */}
           <div className="w-full flex flex-col space-y-3 pt-7">
             <Link href="/about">
-              <button className="w-full px-4 py-3 my-5 rounded-lg bg-#02120F backdrop-blur border border-4 border-white/20 text-white text-xl shadow-md hover:bg-white/20 hover:shadow-lg transition duration-200 font-serif">
+              <button className="w-full px-4 py-3 my-5 rounded-lg bg-[#02120F] backdrop-blur border border-4 border-white/20 text-white text-xl shadow-md hover:bg-white/20 hover:shadow-lg transition duration-200 font-serif">
                 About
               </button>
             </Link>
             <Link href="/cart">
-              <button className="w-full px-4 py-3 my-5 rounded-lg bg-#02120F backdrop-blur border border-4 border-white/20 text-white text-xl shadow-md hover:bg-white/20 hover:shadow-lg transition duration-200 font-serif">
+              <button className="w-full px-4 py-3 my-5 rounded-lg bg-[#02120F] backdrop-blur border border-4 border-white/20 text-white text-xl shadow-md hover:bg-white/20 hover:shadow-lg transition duration-200 font-serif">
                 Cart
               </button>
             </Link>
             <Link href="/contact">
-              <button className="w-full px-4 py-3 my-5 rounded-lg bg-#02120F backdrop-blur border border-4 border-white/20 text-white text-xl shadow-md hover:bg-white/20 hover:shadow-lg transition duration-200 font-serif">
+              <button className="w-full px-4 py-3 my-5 rounded-lg bg-[#02120F] backdrop-blur border border-4 border-white/20 text-white text-xl shadow-md hover:bg-white/20 hover:shadow-lg transition duration-200 font-serif">
                 Contact
               </button>
             </Link>
           </div>
         </aside>
-
         {/* Gallery Photos */}
-        <div className="flex-1 min-h-0 m-0">
-          <div className="w-full h-full relative flex items-center justify-center bg-[#02120f]">
-            <Image
-              key={images[currentIndex].src}
-              src={images[currentIndex].src}
-              alt={images[currentIndex].alt}
-              fill
-              className="rounded shadow-md object-contain h-full transition-opacity duration-1000"
-            />
+        <div className="flex-1 min-h-0 bg-[#02120f] overflow-auto">
+          <div className="columns-2 md:columns-3 gap-2 p-4 bg-[#02120f]">
+            {images.map((image) => (
+              <div key={image.src} className="mb-4 break-inside-avoid">
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  width={600}
+                  height={400}
+                  className="w-full h-auto rounded-lg shadow-md"
+                />
+              </div>
+            ))}
           </div>
         </div>
       </section>
     </div>
-  );
-}
+  
+  )};
+
